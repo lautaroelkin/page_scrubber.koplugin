242fe76da01ade1afea5a1ffd70911938c27ed58
lautaroelkin <101447265+lautaroelkin@users.noreply.github.com>
2026-09-01 14:49:34 -0300

    Update _meta.lua

--- page_scrubber.koplugin/_meta.lua
+++ page_scrubber.koplugin/_meta.lua
@@ -2,6 +2,6 @@ return {
     name = "page_scrubber",
     fullname = "Page Scrubber",
     description = "Advanced page scrubber overlay with split-screen, smart tabs, and gesture support.",
-    version = "4.0.1",
+    version = "4.1.1",
     author = "Lautaro",
 }
