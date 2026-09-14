8737cc52f83b73eebf4d2f19a8352519f194416a
lautaroelkin <101447265+lautaroelkin@users.noreply.github.com>
2026-09-14 08:56:52 -0300

    Update _meta.lua

--- _meta.lua
+++ _meta.lua
@@ -2,6 +2,6 @@ return {
     name = "page_scrubber",
     fullname = "Page Scrubber",
     description = "Advanced page scrubber overlay with split-screen, smart tabs, and gesture support.",
-    version = "6.2.0",
+    version = "6.2.1",
     author = "Lautaro",
 }
